eb7efc0e59b90f464c966cea8fc4649a6c4b2d62
Miguel Chenge <miguelchenge@ITs-MacBook-Pro.local>
2023-02-14 14:14:30 -0600

    Fibonacci excersise

---
+++ src/main/scala/basic/exercises/IntroductionExercise.scala
@@ -0,0 +1,19 @@
+package basic.exercises
+
+object IntroductionExercise extends App {
+  /*
+  * 1. Create a function called fibonacciSequence which receive 1 integer parameter (The position in the fibonacci sequence)
+  * 2. The idea of this function is to return the number in the fibonacci sequence
+  * The fibonacci sequence is the series of numbers where each number is the sum of the two preceding numbers
+  * Example 0,1,1,2,3,5,8,13,etc
+  *
+  * Tests:
+  * 1. fibonacciSequence(5) -> 3
+  * 2. fibonacciSequence(8) -> 13
+  * 3. fibonacciSequence(16) -> 610
+  *
+  * Extra Small Challenge if you want:
+  * Iterate from 1 to 20 and find the number in each of the positions of the fibonacci sequence
+  * */
+
+}
