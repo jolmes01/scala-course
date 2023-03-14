e5f1ad56317d206d1643715bcd98e5196ea3308b
Miguel Chenge <miguelchenge@Apex-Standard-C02GC5MBMD6M.local>
2023-03-14 08:38:41 -0600

    Main methods in Scala 2

---
+++ src/main/scala/basic/introduction/MainsInScala.scala
@@ -0,0 +1,13 @@
+package basic.introduction
+
+object MainsInScala{
+  def main(args: Array[String]): Unit = {
+    println("Hello from main of class",args)
+  }
+
+}
+
+object MainsInScala2 extends App {
+  println("Hello from main of class",args)
+}
+
